75899d3f321f791042a46fa649d4e75b132bfddb
vagrant <vagrant@localhost.localdomain>
2020-08-17 14:08:51 +0000

    jQ[Add]jQjQuery練習

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,16 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(function(){
+  $(".jquery").on("click", function(){
+    $(".jquery").addClass("jquery-add");
+  });
+  $(".jquery").mouseout(function(){
+    $(".jquery").removeClass("jquery-add");
+  });
+});
